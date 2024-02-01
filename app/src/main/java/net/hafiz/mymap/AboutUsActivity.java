42d778652a6d913132cc34a1b07e50ee19173d6c
Darfriz <aliff.darfriz@gmail.com>
2024-02-02 00:25:24 +0800

    V10.8 - Github Link

--- app/src/main/java/net/hafiz/mymap/AboutUsActivity.java
+++ app/src/main/java/net/hafiz/mymap/AboutUsActivity.java
@@ -1,7 +1,12 @@
 package net.hafiz.mymap;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutUsActivity extends AppCompatActivity {
@@ -10,10 +15,27 @@ public class AboutUsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
-        // Set up your About Us page layout here
 
         // Enable the back button in the action bar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        TextView urlTextView = findViewById(R.id.urlTextView);
+
+        // Set a click listener on the URL TextView
+        urlTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Open the GitHub URL in a browser
+                openUrl("https://github.com/Darfriz/cliniclocator/tree/main");
+            }
+        });
+    }
+
+    // Method to open a specific URL
+    private void openUrl(String url) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
     }
 
     @Override
